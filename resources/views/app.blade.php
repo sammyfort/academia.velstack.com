53dc068bfc47153543e9d3cf7608688c02b1ec4d
Emmanuel Arhinful <kofibusy@gmail.com>
2025-08-20 19:29:03 +0000

    chat widget

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,21 @@
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
+
+
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/664fd84a981b6c564773c4f0/1huju9p3e';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
+
+
     </head>
     <body class="antialiased">
         @inertia
